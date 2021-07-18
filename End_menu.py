059663433b9ba01ef0582a5b6bc74bb6dd88971d
Alexandre Ciorascu <alexandre.ciorascu@u-psud.fr>
2021-07-18 15:36:33 +0200

    new selection for endmenu

--- End_menu.py
+++ End_menu.py
@@ -16,18 +16,30 @@ class End_menu:
         self.title_rect.x = game.width / 2 - self.title_rect.width / 2
         self.title_rect.y = game.height / 6
 
-        self.restart = pygame.image.load('assets/buttons/restart.png').convert_alpha()
-        self.restart = pygame.transform.scale(self.restart, (200, 200))
-        self.restart_rect = self.restart.get_rect()
-        self.restart_rect.x = self.restart_rect.width / 2
-        self.restart_rect.y = game.height / 2
+        self.restart1 = pygame.image.load('assets/buttons/restart1.png').convert_alpha()
+        self.restart1 = pygame.transform.scale(self.restart1, (300, 200))
+        self.restart_rect1 = self.restart1.get_rect()
+        self.restart_rect1.x = self.restart_rect1.width / 2
+        self.restart_rect1.y = game.height / 2
 
-        self.menu = pygame.image.load('assets/buttons/menu.png').convert_alpha()
-        self.menu = pygame.transform.scale(self.menu, (200, 200))
-        self.menu_rect = self.menu.get_rect()
-        self.menu_rect.x = game.width - 3 * self.menu_rect.width/2
-        self.menu_rect.y = game.height / 2
+        self.restart2 = pygame.image.load('assets/buttons/restart2.png').convert_alpha()
+        self.restart2 = pygame.transform.scale(self.restart2, (300, 200))
+        self.restart_rect2 = self.restart2.get_rect()
+        self.restart_rect2.x = self.restart_rect2.width / 2
+        self.restart_rect2.y = game.height / 2
 
+        self.menu1 = pygame.image.load('assets/buttons/menu1.png').convert_alpha()
+        self.menu1 = pygame.transform.scale(self.menu1, (300, 200))
+        self.menu_rect1 = self.menu1.get_rect()
+        self.menu_rect1.x = game.width - 3 * self.menu_rect1.width / 2
+        self.menu_rect1.y = game.height / 2
+
+        self.menu2 = pygame.image.load('assets/buttons/menu2.png').convert_alpha()
+        self.menu2 = pygame.transform.scale(self.menu2, (300, 200))
+        self.menu_rect2 = self.menu2.get_rect()
+        self.menu_rect2.x = game.width - 3 * self.menu_rect2.width / 2
+        self.menu_rect2.y = game.height / 2
+        self.selected = 0
         self.music_is_playing = False
 
     def end_menu(self, screen):
@@ -37,24 +49,39 @@ class End_menu:
             pygame.mixer.music.set_volume(0.3)
             self.music_is_playing = True
         self.game.all_enemies.empty()
-        screen.blit(self.restart, self.restart_rect)
         screen.blit(self.title, self.title_rect)
-        screen.blit(self.menu, self.menu_rect)
+        if self.selected == 0:
+            screen.blit(self.menu1, self.menu_rect1)
+            screen.blit(self.restart2, self.restart_rect2)
+        else:
+            screen.blit(self.menu2, self.menu_rect2)
+            screen.blit(self.restart1, self.restart_rect1)
+
         for event in pygame.event.get():
             # detection de la fermeture de la fenetre
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            # détection de pression d'une touche
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                if self.restart_rect.collidepoint(event.pos):
-                    self.game.is_playing = True
-                    self.game.is_dead = False
-                    self.music_is_playing = False
-                    pygame.mixer.music.stop()
-                    self.game.new_game()
-                if self.menu_rect.collidepoint(event.pos):
-                    self.game.is_playing = False
-                    self.game.is_dead = False
-                    self.music_is_playing = False
-                    pygame.mixer.music.stop()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    if self.selected == 0:
+                        self.game.is_playing = True
+                        self.game.is_dead = False
+                        self.music_is_playing = False
+                        pygame.mixer.music.stop()
+                        self.game.new_game()
+                    else:
+                        self.game.is_playing = False
+                        self.game.is_dead = False
+                        self.music_is_playing = False
+                        pygame.mixer.music.stop()
+                elif event.key == pygame.K_RIGHT:
+                    if self.selected == 0:
+                        self.selected = 1
+                    else:
+                        break
+                elif event.key == pygame.K_LEFT:
+                    if self.selected == 1:
+                        self.selected = 0
+                    else:
+                        break
